e390dce6638c1d34de905ba51cd9e01e19de2ccf
Ritik Kumar Gupta <ririkkumargupta@gmail.com>
2026-02-21 15:36:38 +0530

    Logic Build

---
+++ Arrays Practice/prectice.js
@@ -0,0 +1,11 @@
+//  for a given array with marks of student ->[85,97,44,37,76,60]
+// find the average marks of the entire class
+
+let marks_student = [85,97,44,37,76,60];
+
+let sum = 0;
+
+for(let val of marks_student) {
+    console.log(val);
+    
+}
